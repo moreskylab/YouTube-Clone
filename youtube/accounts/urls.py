b0a31be7d2c5e817059b4e9997f37e728efedc71
Ankush More <ankushitguy@gmail.com>
2026-01-09 21:11:03 +0530

    changes and errors resolved

--- youtube/accounts/urls.py
+++ youtube/accounts/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("register/",views.RegisterView.as_view(), name="register"),
+    path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", auth_views.LoginView.as_view(
         template_name="accounts/login.html",
         redirect_authenticated_user=True,
     ), name="login"),
-    path("logout/",auth_views.LogoutView.as_view(
-        template_name="accounts/login.html",
-    ),name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(
+        template_name="accounts/logout.html",
+    ), name="logout"),
 ]
--- youtube/videos/imagekit_client.py
+++ youtube/videos/imagekit_client.py
@@ -1,10 +1,50 @@
 import os
-from http.client import responses
-
 from imagekitio import ImageKit
+
 
 def get_imagekit_client():
     return ImageKit()
+
+
+def _get_watermark_transformation(username: str):
+    return (
+        "l-text,",
+        f"i-{username},",
+        "lfo-bottom_left,",
+        "lx-10,ly-10,",
+        "fs-32,",
+        "co-FFFFFF,",
+        "bg-00000060,",
+        "pa-4_8,",
+        "l-end"
+    )
+
+
+def get_optimized_video_url(base_url: str) -> str:
+    if "?" in base_url:
+        return f"{base_url}&tr=q-50,f-auto"
+    return f"{base_url}?tr=q-50,f-auto"
+
+
+def get_streaming_url(base_url: str) -> str:
+    return f"{base_url}/ik-master.m3u8?tr=sr-240_360_480_720_1080"
+
+
+def get_thumbnail_url(
+        base_url: str, username: str = None
+) -> str:
+    transformations = "".join(_get_watermark_transformation(username))
+
+    return f"{base_url}/ik-thumbnail.jpg?tr={transformations}"
+
+
+def add_image_watermark(
+        base_url: str, username: str = None
+) -> str:
+    transformations = "".join(_get_watermark_transformation(username))
+
+    return f"{base_url}?tr={transformations}"
+
 
 def upload_video(file_data: bytes, file_name: str, folder: str = "videos") -> dict:
     public_key = os.environ.get("IMAGEKIT_PUBLIC_KEY")
@@ -23,12 +63,13 @@ def upload_video(file_data: bytes, file_name: str, folder: str = "videos") -> di
         "url": response.url
     }
 
+
 def upload_thumbnail(file_data: bytes, file_name: str, folder: str = "thumbnails") -> dict:
     import base64
     public_key = os.environ.get("IMAGEKIT_PUBLIC_KEY")
 
     if file_data.startswith("data:"):
-        base64_data = file_data.split(",",1)[1]
+        base64_data = file_data.split(",", 1)[1]
         image_bytes = base64.b64decode(base64_data)
     else:
         image_bytes = base64.b64decode(file_data)
@@ -47,3 +88,8 @@ def upload_thumbnail(file_data: bytes, file_name: str, folder: str = "thumbnails
         "url": response.url
     }
 
+
+def delete_video(file_id: str) -> bool:
+    client = get_imagekit_client()
+    client.files.delete(file_id=file_id)
+    return True
